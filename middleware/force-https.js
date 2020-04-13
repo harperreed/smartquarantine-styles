662b035e36edfc87fc5891d4022c30ee284c0629
Harper Reed <harper@nata2.org>
2020-04-12 19:33:28 -0500

    let's not force https

--- middleware/force-https.js
+++ middleware/force-https.js
@@ -6,6 +6,7 @@ const enforce = require('express-sslify')
 const isDev = process.env.NODE_ENV === 'development'
 
 // redirect http to https for all requests. useful for our sample deploy.
+/*
 if (!isDev) {
   router.use(
     enforce.HTTPS({trustProtoHeader: true})
@@ -13,3 +14,4 @@ if (!isDev) {
 }
 
 exports.preload = router
+*/
